603f8e1a7d05abad9e1c97a113bd2347f656b9ec
Matt Harding <matt@wherethehellismatt.com>
2018-07-18 10:48:48 -0700

    happiness

--- lifesim.js
+++ lifesim.js
@@ -1,104 +1,126 @@
 'use strict';
 
-(function(module) {
+// create all the variables
+var ageYears = 18;
+var ageMonths = 0;
+var income = 4000;
+var takeHome = income * .75;
+var rent = 750;
+var utilities = income * .1;
+var food = 1200;
+var entertainment = 1000;
+var savings = 0;
+var savingsHappiness = savings;
+var ccDebt = 0;
+var ccDebtHappiness = ccDebt;
+var studentDebt = 40000;
+var studentDebtHappiness = studentDebt;
+var happiness = 50;
 
-    function Book (rawDataObj) {
-      this.book_id = rawDataObj.book_id;
-      this.title = rawDataObj.title;
-      this.author = rawDataObj.author;
-      this.isbn = rawDataObj.isbn;
-      this.image_url = rawDataObj.image_url;
-      this.description = rawDataObj.description;
+// display all the info
+document.getElementById('ageValue').innerHTML = ageYears + " Years " + ageMonths + " Months";
+document.getElementById('incomeValue').innerHTML = income.toFixed(2);
+document.getElementById('takeHomeValue').innerHTML = takeHome.toFixed(2);
+document.getElementById('rentValue').innerHTML = rent.toFixed(2);
+document.getElementById('utilitiesValue').innerHTML = utilities.toFixed(2);
+document.getElementById('foodValue').innerHTML = food.toFixed(2);
+document.getElementById('entValue').innerHTML = entertainment.toFixed(2);
+document.getElementById('savingsValue').innerHTML = savings.toFixed(2);
+document.getElementById('ccDebtValue').innerHTML = ccDebt.toFixed(2);
+document.getElementById('studentDebtValue').innerHTML = studentDebt.toFixed(2);
+document.getElementById('happinessValue').innerHTML = happiness;
+
+// advance the counter by a month
+function advanceMonth() {
+    ageMonths++
+ // reset the year
+    if(ageMonths > 11)
+    {
+        ageYears++;
+        ageMonths = 0;
+        // annual raise
+        income = income * 1.05;
+        takeHome = income * .75;
+        document.getElementById('incomeValue').innerHTML = income.toFixed(2);
+        document.getElementById('takeHomeValue').innerHTML = takeHome.toFixed(2);    
     }
-  
-    Book.all = [];
-  
-    Book.prototype.toHtml = function() {
-      let template = Handlebars.compile($('#book-list-template').text());
-      return template(this);
+    // pay bills and add/subtract remainder to savings
+    savings = savings + takeHome - rent - utilities - food - entertainment;
+    // calculate cc debt
+    ccDebt = ccDebt * (1 + 17/1200);
+    // put shortfall on credit card
+    if(savings < 0)
+    {
+        ccDebt = ccDebt + (0 - savings);
+        savings = 0;
     }
-  
-    Book.prototype.showDetails = function() {
-      let template = Handlebars.compile($('#book-detail-template').text());
-      return template(this);
+    // calculate student loan debt
+    studentDebt = studentDebt * (1 + 4/1200);
+    // update values
+    document.getElementById('ageValue').innerHTML = ageYears + " Years " + ageMonths + " Months";
+    document.getElementById('savingsValue').innerHTML = savings.toFixed(2);
+    document.getElementById('ccDebtValue').innerHTML = ccDebt.toFixed(2);
+    document.getElementById('studentDebtValue').innerHTML = studentDebt.toFixed(2);
+
+    // adjust happiness
+    if (savings > (savingsHappiness + 1000))
+    {    
+        happiness++;
+        savingsHappiness = savings;
+        document.getElementById('happinessValue').innerHTML = happiness;
+    } else if (savings < (savingsHappiness - 1000))
+    {
+        happiness--;
+        savingsHappiness = savings;
+        document.getElementById('happinessValue').innerHTML = happiness;
     }
-  
-    Book.prototype.insertBook = function(callback) {
-      $.post(`${ENV.apiUrl}/api/v1/books`, {
-        title: this.title,
-        author: this.author,
-        isbn: this.isbn,
-        image_url: this.image_url,
-        description: this.description})
-        .then(callback);
-      console.log('Sent to server');
+    if (ccDebt > (ccDebtHappiness + 1000))
+    {    
+        happiness--;
+        ccDebtHappiness = ccDebt;
+        document.getElementById('happinessValue').innerHTML = happiness;
+    } else if (ccDebt < (ccDebtHappiness - 1000))
+    {
+        happiness++;
+        ccDebtHappiness = ccDebt;
+        document.getElementById('happinessValue').innerHTML = happiness;
     }
-  
-    Book.prototype.deleteBook = function(callback) {
-      $.ajax({
-        url: `/api/v1/books/${this.book_id}`,
-        method: 'DELETE'
-      })
-        .then(console.log)
-        .then(callback);
-    };
-  
-    Book.loadAll = rows => {
-      Book.all = rows.sort((a, b) => {return a.title > b.title;})
-        .map(instance => new Book(instance));
+    if (studentDebt > (studentDebtHappiness + 1000))
+    {    
+        happiness--;
+        studentDebtHappiness = studentDebt;
+        document.getElementById('happinessValue').innerHTML = happiness;
+    } else if (studentDebt < (studentDebtHappiness - 1000))
+    {
+        happiness++;
+        studentDebtHappiness = studentDebt;
+        document.getElementById('happinessValue').innerHTML = happiness;
     }
-  
-    Book.fetchAll = callback => {
-      $.get(`${ENV.apiUrl}/api/v1/books`)
-        .then(data => {
-          Book.loadAll(data);
-          if(callback) callback();
-        },
-        err => {app.errorView.errorCallback(err);}
-        )
+}
+
+// transfer savings to credit card
+function payCCDebt() {
+    if(ccDebt >= savings) {
+        ccDebt = ccDebt - savings;
+        savings = 0;
+    } else {
+        savings = savings - ccDebt;
+        ccDebt = 0;
     }
-  
-    Book.fetchOne = (ctx, next) => {
-      $.get(`${ENV.apiUrl}/api/v1/books/${ctx.params.book_id}`)
-        .then(() => {
-          next();
-        },
-        err => {
-          app.errorView.errorCallback(err);
-        })
+
+    document.getElementById('savingsValue').innerHTML = savings.toFixed(2);
+    document.getElementById('ccDebtValue').innerHTML = ccDebt.toFixed(2);
+}
+
+// transfer savings to student loan
+function payStudentDebt() {
+    if(studentDebt >= savings) {
+        studentDebt = studentDebt - savings;
+        savings = 0;
+    } else {
+        savings = savings - studentDebt;
+        studentDebt = 0;
     }
-  
-    Book.create = (event) => {
-      event.preventDefault();
-      var book = new Book({
-        title: $('#book-title').val(),
-        author: $('#book-author').val(),
-        isbn: $('#book-isbn').val(),
-        image_url: $('#book-image-url').val(),
-        description: $('#book-description').val()
-      })
-      book.insertBook();
-    }
-  
-    Book.update = (event) => {
-      event.preventDefault();
-      console.log(event.target);
-      $.ajax({
-        url: `/api/v1/books/${this}`,
-        method: 'PUT',
-        data: {
-          title: $('#update-book-title').val(),
-          author: $('#update-book-author').val(),
-          isbn: $('#update-book-isbn').val(),
-          image_url: $('#update-book-image-url').val(),
-          description: $('#update-book-description').val()
-        }
-      })
-        .then(data => {
-          console.log(data);
-        })
-    }
-  
-    module.Book = Book;
-  
-  });
+    document.getElementById('savingsValue').innerHTML = savings.toFixed(2);
+    document.getElementById('studentDebtValue').innerHTML = studentDebt.toFixed(2);
+};
